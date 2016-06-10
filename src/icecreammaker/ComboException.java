a685183f1c935aff3e3497a5e9854d5c85b710f4
Enzo <enzocasamassima@gmail.com>
2016-06-10 18:54:47 -0400

    Initial Commit

---
+++ src/icecreammaker/ComboException.java
@@ -0,0 +1,11 @@
+package icecreammaker;
+
+public class ComboException extends Exception {
+	
+private static final long serialVersionUID = 1L;
+
+	public ComboException(String msg) {
+		super(msg);
+	}
+
+}
